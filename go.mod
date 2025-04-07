bb1213361e1528701116e93c2abf0caf3082cad4
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-04-07 19:41:42 -0300

    Add basic tea flow

--- go.mod
+++ go.mod
@@ -4,11 +4,28 @@ go 1.24.1
 
 require (
 	github.com/Nishivaly/go-reddit/v2 v2.0.0-20250407045224-c25e9df3921e
+	github.com/charmbracelet/bubbletea v1.3.4
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/oauth2 v0.29.0
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.3.8 // indirect
 )
--- main.go
+++ main.go
@@ -3,26 +3,103 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/Nishivaly/go-reddit/v2/reddit"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main() {
+type model struct {
+	client *reddit.Client
+	posts  []*reddit.Post
+	err    error
+}
+
+type redditClientMsg struct {
+	client *reddit.Client
+}
+
+type redditPostsMsg struct {
+	posts []*reddit.Post
+}
+
+type errMsg struct{ err error }
+
+func (e errMsg) Error() string { return e.err.Error() }
+
+func initRedditClient() tea.Msg {
 	client, err := getRedditClient()
 	if err != nil {
-		panic(err)
+		return errMsg{err}
+	}
+	return redditClientMsg{client}
+}
+
+func getPosts(client *reddit.Client) tea.Cmd {
+	return func() tea.Msg {
+		posts, _, err := client.Subreddit.TopPosts(context.Background(), "golang", &reddit.ListPostOptions{
+			ListOptions: reddit.ListOptions{
+				Limit: 5,
+			},
+			Time: "all",
+		})
+		if err != nil {
+			return errMsg{err}
+		}
+		return redditPostsMsg{posts}
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return initRedditClient
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case redditClientMsg:
+		m.client = msg.client
+		return m, getPosts(m.client)
+
+	case redditPostsMsg:
+		m.posts = msg.posts
+		return m, nil
+
+	case errMsg:
+		m.err = msg.err
+
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
 	}
 
-	posts, _, err := client.Subreddit.TopPosts(context.Background(), "golang", &reddit.ListPostOptions{
-		ListOptions: reddit.ListOptions{
-			Limit: 5,
-		},
-		Time: "all",
-	})
-	if err != nil {
-		log.Fatal(err)
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble: %s\n\n", m.err.Error())
 	}
 
-	fmt.Printf("Received %d posts.\n", len(posts))
+	s := fmt.Sprintf("Connecting bruh")
+
+	if m.client != nil {
+		s += fmt.Sprint("\nconnecto bruh")
+	}
+
+	if m.posts != nil {
+		s += "\nPosts:\n"
+		for i, post := range m.posts {
+			s += fmt.Sprintf("%d. %s\n", i+1, post.Title)
+		}
+	}
+	return "\n" + s + "\n\n"
+}
+
+func main() {
+	if _, err := tea.NewProgram(model{}).Run(); err != nil {
+		fmt.Printf("Uh oh, there was an error: %v\n", err)
+		os.Exit(1)
+	}
 }
